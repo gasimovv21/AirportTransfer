6b80f7db8818ac2dffc47b6090a67f648cef3c0b
gasimovv21 <gasimoweltun@gmail.com>
2024-01-16 22:20:37 +0100

    added base url to path for features icons

--- backend_api/api_v1/serializers.py
+++ backend_api/api_v1/serializers.py
@@ -3,15 +3,10 @@ from cars.models import Feature, Photo, Car
 
 
 class FeatureSerializer(serializers.ModelSerializer):
-    icon = serializers.SerializerMethodField()
 
     class Meta:
         model = Feature
         fields = ['name', 'icon']
-    
-
-    def get_icon(self, obj):
-        return obj.icon.url if obj.icon else None
 
 
 class PhotoSerializer(serializers.ModelSerializer):
